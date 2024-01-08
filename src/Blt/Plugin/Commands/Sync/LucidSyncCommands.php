c50c294796aa509f642fc7d0cee517009a1c4f7f
Thomas Hafner <t.hafner@claritypartners.com>
2024-01-08 12:28:23 -0600

    additional trait updates.

--- src/Blt/Plugin/Commands/Sync/LucidSyncCommands.php
+++ src/Blt/Plugin/Commands/Sync/LucidSyncCommands.php
@@ -3,18 +3,17 @@
 namespace Thafner\Lucid\Blt\Plugin\Commands\Sync;
 
 use Acquia\Blt\Robo\BltTasks;
-use Acquia\Blt\Robo\Exceptions\BltException;
-use AsyncAws\S3\S3Client;
-use Robo\Exception\TaskException;
-use Robo\Result;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Thafner\Lucid\Blt\Plugin\Traits\DatabaseTrait;
 
 /**
  * Defines commands in the "sync" namespace.
  */
 class LucidSyncCommands extends BltTasks {
+
+  use DatabaseTrait;
 
   /**
    * Copies remote db to local db for default site.
@@ -28,39 +27,15 @@ class LucidSyncCommands extends BltTasks {
    */
   public function syncDb(InputInterface $input, OutputInterface $output) {
     $name = $this->getConfigValue('project.machine_name');
+    $bucket = $this->getConfigValue('lucid.database.s3_bucket');
+    $key = $this->getConfigValue('lucid.database.s3_key_prefix_string');
+
     $io = new SymfonyStyle($input, $output);
     $io->title('Syncing database from S3 bucket for ' . $name . '.');
 
+    $this->databaseDownload($name, $bucket, $key, $input, $output);
 
 
-    $awsConfigDirPath = getenv('HOME') . '/.aws';
-    $awsConfigFilePath = "$awsConfigDirPath/credentials";
-    if (!is_dir($awsConfigDirPath) || !file_exists($awsConfigFilePath)) {
-      $result = $this->configureAwsCredentials($awsConfigDirPath, $awsConfigFilePath, $input, $output);
-      if ($result->wasCancelled()) {
-        return Result::cancelled();
-      }
-    }
-
-    $s3 = new S3Client([
-      'region' => 'us-east-2',
-    ]);
-
-    $downloadFileName = 'cclerkdevDrupal9.sql.gz';
-
-    if (file_exists($downloadFileName)) {
-      $this->say("Skipping download. Latest database dump file exists >>> $downloadFileName");
-    } else {
-      $bucket = $this->getConfigValue('lucid.database.s3_bucket');
-      $key = $this->getConfigValue('lucid.database.s3_key_prefix_string');
-      $result = $s3->getObject([
-        'Bucket' => $bucket,
-        'Key' => $key,
-      ]);
-      $metadata = json_decode($result->getBody()->getContentAsString());
-      $this->say("Database dump file downloaded >>> $metadata");
-    }
-    return $downloadFileName;
 
   }
 
@@ -146,38 +121,7 @@ class LucidSyncCommands extends BltTasks {
     return $result;
   }
 
-  /**
-   * Configure AWS credentials.
-   *
-   * @param string $awsConfigDirPath
-   *   Path to the AWS configuration directory.
-   * @param string $awsConfigFilePath
-   *   Path to the AWS configuration file.
-   */
-  protected function configureAwsCredentials(string $awsConfigDirPath, string $awsConfigFilePath, InputInterface $input, OutputInterface $output): Result
-  {
-    $io = new SymfonyStyle($input, $output);
-    $yes = $io->confirm('AWS S3 credentials not detected. Do you wish to configure them?');
-    if (!$yes) {
-      return Result::cancelled();
-    }
 
-    if (!is_dir($awsConfigDirPath)) {
-      $this->_mkdir($awsConfigDirPath);
-    }
-
-    if (!file_exists($awsConfigFilePath)) {
-      $this->_touch($awsConfigFilePath);
-    }
-
-    $awsKeyId = $this->ask("AWS Access Key ID:");
-    $awsSecretKey = $this->askHidden("AWS Secret Access Key:");
-    return $this->taskWriteToFile($awsConfigFilePath)
-      ->line('[default]')
-      ->line("aws_access_key_id = $awsKeyId")
-      ->line("aws_secret_access_key = $awsSecretKey")
-      ->run();
-  }
 
 
 
---
+++ src/Blt/Plugin/Traits/DatabaseTrait.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Thafner\Lucid\Blt\Plugin\Traits;
+
+use AsyncAws\S3\S3Client;
+use AsyncAws\Core\Exception\Exception;
+use Acquia\Blt\Robo\Exceptions\BltException;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Robo\Result;
+
+trait DatabaseTrait {
+
+  public function databaseDownload(string $site_name, string $bucket, string $key, InputInterface $input, OutputInterface $output): string|Result
+  {
+    $io = new SymfonyStyle($input, $output);
+    $io->title('Syncing database');
+    $awsConfigDirPath = getenv('HOME') . '/.aws';
+    $awsConfigFilePath = "$awsConfigDirPath/credentials";
+    if (!is_dir($awsConfigDirPath) || !file_exists($awsConfigFilePath))
+    {
+      $result = $this->configureAwsCredentials($awsConfigDirPath, $awsConfigFilePath, $input, $output);
+      if ($result->wasCancelled())
+      {
+        return Result::cancelled();
+      }
+    }
+
+    $s3 = new S3Client([
+      'region' => 'us-east-2',
+    ]);
+
+    $downloadFileName = 'cclerkdevDrupal9.sql.gz';
+
+    if (file_exists($downloadFileName)) {
+      $io->text([
+        "Skipping download.",
+        "Latest database dump file exists.",
+        $downloadFileName,
+      ]);
+    } else {
+      $io->progressStart(10);
+
+      try {
+        $result = $s3->getObject([
+          'Bucket' => $bucket,
+          'Key' => $key,
+        ]);
+        $io->progressStart();
+      } catch (Exception $e) {
+        $io->error([
+          "There was an error downloading the database.",
+          $e->getCode(),
+          $e->getMessage(),
+        ]);
+      }
+      $io->progressAdvance(2);
+      $fp = fopen('/app/' . $downloadFileName, 'wb');
+      $io->progressAdvance(5);
+      stream_copy_to_stream($result->getBody()->getContentAsResource(), $fp);
+      $io->progressFinish();
+      $io->text("Database successfully downloaded.");
+    }
+    return $downloadFileName;
+  }
+
+  /**
+   * Configure AWS credentials.
+   *
+   * @param string $awsConfigDirPath
+   *   Path to the AWS configuration directory.
+   * @param string $awsConfigFilePath
+   *   Path to the AWS configuration file.
+   */
+  protected function configureAwsCredentials(string $awsConfigDirPath, string $awsConfigFilePath, InputInterface $input, OutputInterface $output): Result
+  {
+    $io = new SymfonyStyle($input, $output);
+    $yes = $io->confirm('AWS S3 credentials not detected. Do you wish to configure them?');
+    if (!$yes) {
+      return Result::cancelled();
+    }
+
+    if (!is_dir($awsConfigDirPath)) {
+      $this->_mkdir($awsConfigDirPath);
+    }
+
+    if (!file_exists($awsConfigFilePath)) {
+      $this->_touch($awsConfigFilePath);
+    }
+
+    $awsKeyId = $this->ask("AWS Access Key ID:");
+    $awsSecretKey = $this->askHidden("AWS Secret Access Key:");
+    return $this->taskWriteToFile($awsConfigFilePath)
+      ->line('[default]')
+      ->line("aws_access_key_id = $awsKeyId")
+      ->line("aws_secret_access_key = $awsSecretKey")
+      ->run();
+  }
+
+}
