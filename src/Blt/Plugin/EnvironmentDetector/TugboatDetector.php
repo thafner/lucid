fd328795009a5e6a41e1326d4e8f063d17eddf3d
Thomas Hafner <t.hafner@claritypartners.com>
2024-01-09 08:49:54 -0600

    minor tugboat updates.

--- src/Blt/Plugin/EnvironmentDetector/TugboatDetector.php
+++ src/Blt/Plugin/EnvironmentDetector/TugboatDetector.php
@@ -17,6 +17,7 @@ class TugboatDetector extends EnvironmentDetector {
    * {@inheritdoc}
    */
   public static function getCiSettingsFile(): string {
+    echo("RETURN1");
     return sprintf('%s/vendor/thafner/lucid/settings/tugboat.settings.php', dirname(DRUPAL_ROOT));
   }
 }
